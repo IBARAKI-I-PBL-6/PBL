9336bf0f0e319dc517f63568d449f1cef207ff83
nabefuta220 <60512119+nabefuta220@users.noreply.github.com>
2021-11-15 19:53:44 +0900

    created constructor

--- src/database.py
+++ src/database.py
@@ -9,13 +9,12 @@ logger = getLogger(__name__)
 class database:
     """
     データベースファイルの操作を行うクラス
-    
     """
-    conn:Connection 
-    c:Cursor
+    conn: Connection
+    c: Cursor
     database_name: str
 
-    def __init__(self,data_name:str):
+    def __init__(self, data_name: str):
         """
         データベースファイルを開く
 
@@ -24,10 +23,10 @@ class database:
             data_neme : str
             開くデータベースファイルの名前
         """
-        self.database_name=data_name
+        self.database_name = data_name
         self.conn = sqlite3.connect(self.database_name)
         logger.info(f"opened : {self.database_name}")
-        self.c=self.conn.cursor()
+        self.c = self.conn.cursor()
 
     def __del__(self):
         """
@@ -35,7 +34,8 @@ class database:
         """
         self.conn.close()
         logger.info(f"closed : {self.database_name}")
-    def excute(self,command:str):
+
+    def execute(self, command: str):
         """
         SQLコマンドを実行する
 
@@ -58,11 +58,49 @@ class database:
         return self.c.fetchall()
 
 
-if __name__=='__main__':
+class datatable(database):
+    """
+    テーブルの操作を行う
+    """
+    table_name: str
+
+    def __init__(self, database_name: str, table_name: str, args: str = None):
+        """
+        コンストラクタ
+        テーブルオブジェクトを作成する
+
+        Paramters
+        ---------
+        database_name :str
+            データベースのファイル名
+        table_name : str
+            ターブルの名前
+        args : str (defalt = None)
+            テーブルの属性
+
+        Raies
+        -----
+        sqlite3.OperationalError
+            argsを指定したときにその文字列が不正な文字列であったとき
+        """
+        super().__init__(database_name)
+        self.table_name = table_name
+
+        if args is not None:
+            try:
+                self.execute(f'CREATE TABLE {table_name} ({args})')
+                logger.info(
+                    f'created : {self.database_name} :: {self.table_name}')
+            except sqlite3.OperationalError as e:
+                if not str(e) == f"table {self.table_name} already exists":
+                    raise e
+
+
+if __name__ == '__main__':
     from logging_setting import set_logger
     set_logger()
-    data = database('test.db')
-    
-    print(data.excute("select * from articles"))
-    data.excute("select")
+    #data = database('test.db')
 
+    #print(data.excute("select * from articles"))
+    # data.excute("select")
+    data2 = datatable('test.db', 'articles', 'a in')
