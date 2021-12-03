4180434905272f8e9f33487920d8d4e7350eaa5e
st18049 <70875400+st18049@users.noreply.github.com>
2021-12-03 13:27:59 +0900

    少し修正

--- database1.py
+++ database1.py
@@ -1,18 +1,18 @@
 import datetime
-import database1    //仮のデータベースからのインポート
+import pbl_raspi_in   //仮のデータベースからのインポート
 import schedule
 import time
 import serial
 
+starttime=8
+endtime=18
 def reset():  //初期化
   date=0
   for i range(1,25)
     warnhour[i]=0
     data[i]=0
 
-def warn(): //警告カウント、シリアル通信
-      database1.warncnt[date.how]++
-      database1.warncnthour++
+def warn(): //シリアル通信
       ser = serial.Serial("COM11", 9600)    //適当なポート番号
       ser.write('1')
       ser.close()
@@ -23,10 +23,12 @@ reset()
 date=datetime.datetime.now()//現在時刻を取得
 while date<endtime: //終了時間まで
     date=datetime.datetime.now()
-    data[date.how]=database1.data
-    if database1.data>10: //適当な人数
+    data[date.how]=pbl_raspi_in.database1_1
+    if pbl_raspi_in.database1_1>10: //適当な人数
+        database2.warncnt[date.how]++
+        database2.warncnthour++
         warn()
     if now_time-starttime==1:
-        if database1.warncnthour>10 || database1.warncnt[date.how] : //適当
+        if database2.warncnthour>10 || database2.warncnt[date.how]>10 : //適当
             warn()
         starttime=datetime.datetime.now()
